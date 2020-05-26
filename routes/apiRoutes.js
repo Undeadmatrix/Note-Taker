418f33272fde9381b72f8333e5658140f672df6c
Chris Little <chrisdlittle@aol.com>
2020-05-25 21:30:24 -0400

    fixed formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,28 @@
 const noteData = require("../db/db.json");
 
-module.exports = function (app) {
-  app.get("/api/notes", (req, res) => {
+module.exports = function (app) 
+{
+  
+  app.get("/api/notes", (req, res) => 
+  {
     res.json(noteData);
   });
 
-  app.post("/api/notes", (req, res) => {
+  app.post("/api/notes", (req, res) => 
+  {
     noteData.push(req.body);
     res.json(noteData);
   });
 
-  app.delete("/api/notes/:id", (req, res) => {
+  app.delete("/api/notes/:id", (req, res) => 
+  {
+
     let noteId = req.params.id;
     let removeIndex;
 
-    for (let i = 0; i < noteData.length; i++) {
-      if (noteData[i].id === noteId) {
+    for (let i = 0; i < noteData.length; i++) 
+    {
+      if(noteData[i].id === noteId) {
         removeIndex = i;
       }
     }
