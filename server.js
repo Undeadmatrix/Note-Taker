606dce5b783c5b60d843abc02469599098f53fcd
Chris Little <chrisdlittle@aol.com>
2020-05-25 20:48:51 -0400

    created server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routing/api-routes.js")(app);
+require("./routing/html-routes.js")(app);
+
+app.listen(PORT, () => {
+  console.log("Server is listening on: http://localhost" + PORT);
+});
